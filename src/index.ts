75ca27035413c603b0f79ed226187d21e1260201
Naxrul Ahmed <devnaxrul@gmail.com>
2025-12-04 22:19:39 +0600

    fix exports

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,7 @@ import Xansql from "./core/Xansql";
 import Model from "./model";
 import xt from "./Types";
 export { Xansql, Model, xt };
+
+export * from "./core/type";
+export * from "./model/type";
+export * from "./Types/types";
